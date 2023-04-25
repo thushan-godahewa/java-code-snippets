93d663807314c987ca775139c875a9af78b63b33
thushan-godahewa <thushan.godahewa@gmail.com>
2023-04-25 15:24:25 +1000

    Added initial Spring Boot artefacts generated via Spring Initializr.

---
+++ java-code-snippets/src/main/java/com/tsg/samples/javacodesnippets/JavaCodeSnippetsApplication.java
@@ -0,0 +1,13 @@
+package com.tsg.samples.javacodesnippets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeSnippetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeSnippetsApplication.class, args);
+	}
+
+}
---
+++ java-code-snippets/src/test/java/com/tsg/samples/javacodesnippets/JavaCodeSnippetsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tsg.samples.javacodesnippets;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeSnippetsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		Assertions.assertTrue(Boolean.TRUE);
+	}
+}
